abce1cb0ce4724ab043c046a753176f7fa7aa2f4
Edwin <el5091@g.rit.edu>
2021-06-08 14:24:43 -0400

    event handling with form submitting

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,21 @@ class SearchBar extends React.Component {
     // We can default the input by using non-empty string
     state = { term: '' };
 
+    // "this.state.term" cannot be used here, nothing is initiated to it
+    // *2* we can use an arrow function so that "this.state.term" is always = search bar <== IN-USE
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
 
     // We DO NOT put on a set of () whenever we pass a callback function to an event handler -- onInputChange
     // Use .toUpperCase() to force all text to caps
+    // *3* We can do the arrow function on onFormSubmit()
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label> Image Search </label>
                         <input
